847476684c00a5d784b6adb3b36c50e17155df07
TheGreatMegalodon <69216391+TheGreatMegalodon@users.noreply.github.com>
2023-04-24 19:37:07 +0100

    Update Invasion-Speedrun.js

--- Invasion-Speedrun.js
+++ Invasion-Speedrun.js
@@ -51,25 +51,25 @@ this.options = {
   map_size: 20,
   max_players: 1,
   crystal_value: 4,
-  lives: 0,
-  maxtierlives: 0,
+  lives: 1,
+  maxtierlives: 1,
   starting_ship_maxed: true,
   weapon_drop: 10,
   weapons_store: false,
+  choose_ship: [301, 302, 303, 304],
   custom_map: map,
   vocabulary: vocabulary,
-  soundtrack: musicApplyed
+  soundtrack: musicApplyed,
+  ships: Spectator_191 = '{"name":"Spectator","level":1.9,"model":1,"size":0.025,"zoom":0.075,"specs":{"shield":{"capacity":[1e-30,1e-30],"reload":[1000,1000]},"generator":{"capacity":[1e-30,1e-30],"reload":[1,1]},"ship":{"mass":1,"speed":[200,200],"rotation":[1000,1000],"acceleration":[1000,1000]}},"bodies":{"face":{"section_segments":100,"angle":0,"offset":{"x":0,"y":0,"z":0},"position":{"x":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],"y":[-2,-2,2,2],"z":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]},"width":[0,1,1,0],"height":[0,1,1,0],"vertical":true,"texture":[6]}},"typespec":{"name":"Spectator","level":1,"model":1,"code":101,"specs":{"shield":{"capacity":[1e-30,1e-30],"reload":[1000,1000]},"generator":{"capacity":[1e-30,1e-30],"reload":[1,1]},"ship":{"mass":1,"speed":[200,200],"rotation":[1000,1000],"acceleration":[1000,1000]}},"shape":[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001],"lasers":[],"radius":0.001}}'
 };
 
 var time = 25;
 var stage = 0;
-var startingGame = false;
+var startingGame = "ended";
 var instrutor_ended = false;
-var crystal_drop = [0].flat(Infinity);
-var points = [0].flat(Infinity);
-var collectibles = [10, 20, 41, 42, 90, 91];
 var nextAlien_Code = 10;
 var nextAlien_Level = 0;
+var collectibles = [10, 20, 41, 42, 90, 91];
 this.tick = function(game) {
   if (game.step % 60 === 0) {
     switch(startingGame) {
@@ -81,15 +81,12 @@ this.tick = function(game) {
         } else {
           time = 0;
           startingGame = 1;
-          stageValues();
           game.addAlien({
             code: nextAlien_Code,
             level: nextAlien_Level,
             x: game.options.map_size * 5,
             y: game.options.map_size * 5,
-            // option
-            points: points[~~(Math.random() * points.length)],
-            crystal_drop: crystal_drop[~~(Math.random() * crystal_drop.length)],
+            crystal_drop: setVariables({order: stage+2, len: 10, gap: 1})[~~(Math.random() * setVariables({order: stage+2, len: 10, gap: 1}).length)],
             weapon_drop: collectibles[~~(Math.random() * collectibles.length)]
           });
         }
@@ -97,9 +94,41 @@ this.tick = function(game) {
       case 1:
         updateScoreboard(game);
         break;
+      case "ended":
+        if (time >= 0) {
+          endScoreboard(game);
+        } else {
+          for (let ship of game.ships) {
+            ship.gameover({
+              "You Won" : ship.name,
+              "-" : "-",
+              "Time Survived" : time,
+              "Aliens Killed" : ship.custom.kills,
+              "Score" : ship.score
+            });
+            ship.set({idle: true});
+          }
+        }
+        break;
     }
   }
 };
+
+function endScoreboard(game) {
+  let Scoreboard = {
+    id: "scoreboard",
+    clickable: false,
+    visible: true,
+    components: [
+      {type: "text", position: [14, 4, 100, 8], value: `Game is ending`, color: "rgb(255,255,255)", align: "left"},
+      {type: "text", position: [72, 4, 100, 8], value: format_time(time--), color: colorStage, align: "left"},
+      {type: "box", position: [10, 15, 80, 1.4], fill: "rgba(155, 155, 155, 0.4)"},
+      {type: "text", position: [0, 45, 100, 10], value: `Communications`, color: "rgb(255,255,255)", align: "center"},
+      {type: "text", position: [0, 57, 100, 10], value: `Over`, color: "rgb(255,255,255)", align: "center"}
+    ]
+  };
+  for (let ship of game.ships) ship.setUIComponent(Scoreboard);
+}
 
 function prepareScoreboard(game) {
   let Scoreboard = {
@@ -151,12 +180,11 @@ this.event = function(event, game) {
     case "alien_destroyed":
       event.killer.custom.kills++;
       if (event.alien.code === 20 && event.alien.level === 3) {
-        startingGame = false;
-        alert(event.ship, `Well Done!`, `You took ${format_time(time)} minutes to finish the game!`, "rgba(55, 255, 55, 0.8)");
+        setTimeout(() => {gameFinished(event.killer)}, 2000);
       } else {
         if (event.alien.code === 20) {
           // Change Stage
-          stageValues();
+          stage++;
           nextAlien_Code = 10;
           nextAlien_Level = event.alien.level+1;
         } else {
@@ -166,21 +194,23 @@ this.event = function(event, game) {
         }
         const alien = aliensInfo[nextAlien_Code];
         alert(event.killer, `Well Done!`, `Next alien:  ${alien.name}, difficulty:  ${alien.difficulty[nextAlien_Level]}`);
-        setTimeout(() => { 
+        const collectible = (nextAlien_Code == 20 || nextAlien_Code == 19 || nextAlien_Code == 15 || nextAlien_Code == 12) ? 12 : collectibles[~~(Math.random() * collectibles.length)];
+        const crystals = (nextAlien_Code == 20 || nextAlien_Code == 19 || nextAlien_Code == 15 || nextAlien_Code == 12) ? setVariables({order: stage+5, len: 10, gap: 1})[~~(Math.random() * setVariables({order: stage+5, len: 10, gap: 1}).length)] : setVariables({order: stage+2, len: 10, gap: 1})[~~(Math.random() * setVariables({order: stage+2, len: 10, gap: 1}).length)];
+        setTimeout(() => {
           game.addAlien({
             code: nextAlien_Code, 
             level: nextAlien_Level, 
             x: game.options.map_size * 5, 
             y: game.options.map_size * 5,
-            // option
-            points: points[~~(Math.random() * points.length)],
-            crystal_drop: crystal_drop[~~(Math.random() * crystal_drop.length)],
-            weapon_drop: collectibles[~~(Math.random() * collectibles.length)]
+            crystal_drop: crystals,
+            weapon_drop: collectible
           });
-          if (nextAlien_Code == 13) {
-            if (nextAlien_Level < 3) game.aliens[0].set({damage: 40, rate: 2});
-            else game.aliens[0].set({damage: 250, rate: 6});
-          }
+          setTimeout(() => {
+            if (nextAlien_Code == 13) {
+              if (nextAlien_Level < 3) game.aliens[0].set({damage: 40, rate: 2});
+              else game.aliens[0].set({damage: 250, rate: 6});
+            }
+          }, 200);
         }, 4000);
       }
       break;
@@ -189,7 +219,7 @@ this.event = function(event, game) {
       break;
     case "ship_destroyed":
       event.ship.gameover({
-        "You lost" : "-",
+        "You Lost" : event.ship.name,
         "-" : "-",
         "Time Survived" : time,
         "Aliens Killed" : event.ship.custom.kills,
@@ -222,7 +252,16 @@ var ship_instructor = function(ship, message, character = "Lucina", delay = 0, h
     instructor_func = function() {
       ship.showInstructor();
       ship.instructorSays(message, character);
-      setTimeout(() => { ship.hideInstructor(), instrutor_ended = true }, hide_after * 650);
+      setTimeout(() => { 
+        ship.hideInstructor();
+        if (!instrutor_ended) {
+          instrutor_ended = true;
+          addObject("MapCenter", MapCenter, {x: 20 * 5, y: 20 * 5, sx: 60, sy: 60});
+        } else {
+          time = 10;
+          startingGame = "ended";
+        }
+      }, hide_after * 650);
     };
   } else {
     instructor_func = function() {
@@ -241,6 +280,17 @@ function prepareShip(ship) {
   ship_instructor(ship, "To win you must kill every aliens without dying", "Zoltar", 6);
   ship_instructor(ship, "Good luck Commander", "Zoltar", 10);
   ship_instructor(ship, "See you at the end!", "Zoltar", 14, 4);
+}
+
+function gameFinished(ship, time = 0) {
+  startingGame = false;
+  ship.set({x: 0, y: 0, type: 191, collider: false, idle: true});
+  ship_instructor(ship, `Well done! Commander`, "Zoltar");
+  ship_instructor(ship, `You successfuly killed all of the aliens.`, "Zoltar", time+=5);
+  ship_instructor(ship, `It took you ${format_time(time)} minutes to finish the game!`, "Zoltar", time+=6);
+  ship_instructor(ship, `you did a great performance.`, "Zoltar", time+=7);
+  ship_instructor(ship, `i wish you the best, and see you next time.`, "Zoltar", time+=5);
+  ship_instructor(ship, `\n\n\n\ncommunications over.`, "Zoltar", time+=6, 5);
 }
 
 function format_time(time) {
@@ -267,11 +317,40 @@ function setVariables(info, list=[], i=0) {
   return list;
 } 
 
-function stageValues() {
-  stage++;
-  crystal_drop.splice(0);
-  points.splice(0);
-  crystal_drop.push(setVariables({order: stage+5, len: 10, gap: 1}));
-  points.push(setVariables({order: stage, len: 10, gap: 1}));
-}
+const MapCenter = {
+  id: "MapCenter",
+  obj: "https://starblast.data.neuronality.com/mods/objects/plane.obj",
+  emissive: "https://raw.githubusercontent.com/TheGreatMegalodon/Dueling-Component/main/Dueling_Component/Aliens_SpeedRun.png",
+};
 
+const lost_sector_aries = {
+  id: "lost_sector_aries",
+  obj: "https://starblast.io/lost_sector/LostSector_Aries_HardEdges.obj",
+  diffuse: "https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Diffuse.jpg",
+  bump: "https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Height.jpg",
+  specular: "https://starblast.io/lost_sector/LostSector_Aries_LostSector_Aries_Specular.jpg",
+  shininess: 10,
+  emissiveColor: 0,
+  specularColor: 0xFFFFFF,
+  transparent: false
+};
+
+game.setObject({
+  id: "lost_sector_aries",
+  type: lost_sector_aries,
+  position: {x:20 * 5, y: 20 * 5, z:-60},
+  scale: {x:3, y:3, z:3},
+  rotation: {x:0, y:0, z:Math.PI}
+});
+
+addObject = function(Name, ID, info) {
+  const defaultInfo = {x: 0, y: 0, sx: 0, sy: 0, rz: 0};
+  info = {...defaultInfo, ...info};
+  game.setObject({
+    id: Name,
+    type: ID,
+    position: {x: info.x, y: info.y, z: -15},
+    scale: {x: info.sx, y: info.sy, z: 0},
+    rotation: {x: Math.PI, y: 0, z: info.rz}
+  });
+};
